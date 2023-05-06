8ab4a994dc3098d7e85b948dabfcad35af3e6caa
aniqabd <aniq.hilman98@gmail.com>
2023-05-06 10:56:03 +0800

    First commit

--- group9fe/src/App.js
+++ group9fe/src/App.js
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import CreateNewClaim from "./Pages/Transactions/CreateNewClaim";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Router>
           <NavBar />
           <Routes>
+            <Route path="/create-new-claim-record" element={<CreateNewClaim />}></Route>
           </Routes>
       </Router>
     </div>
