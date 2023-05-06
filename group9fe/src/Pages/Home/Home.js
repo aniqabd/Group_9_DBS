f445ec5d795ec487d75fe955c9fd26dacbe8ec9b
cmxwc <59865108+cmxwc@users.noreply.github.com>
2023-05-06 12:19:43 +0800

    Add colour and link

--- group9fe/src/Pages/Home/Home.js
+++ group9fe/src/Pages/Home/Home.js
@@ -1,54 +1,62 @@
 import Table from 'react-bootstrap/Table';
 import { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom';
 
+import './Home.css'
 
 export default function Home() {
-    const [ countries, setCountries ] = useState([]);
+    const [ allClaims, setClaims ] = useState([]);
 
     useEffect(() => {
-    const fetchata = async () => {
+    const fetchdata = async () => {
 
         const response = await fetch(
             'https://api.sampleapis.com/countries/countries');
             const data = await response.json();
-            setCountries( data.slice( 0,3) )
+            setClaims( data.slice( 0,3) )
         
     }
-
-    // Call the function
-    fetchata();
+    fetchdata();
     }, []);
 
+    const getColor = (status) => {
+        if (status == "Rejected") return 'red';
+        if (status == "Approved") return 'green';
+        return '';
+    };
+
     return (
-    <>
-        <Table striped bordered hover>
-        <thead>
-            <tr>
-            <th>Claim Id</th>
-            <th>Project Id</th>
-            <th>Currency</th>
-            <th>Status of Claim</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                countries.map( (country,key) =>
-                <tr key={key}>
-                    <td className='table-data'>{country.name }</td>
-                    <td className='table-data'>{country.capital }</td>
-                    <td className='table-data'><img width='20px' height='10px'
-                            src={country.media.flag} alt="flag" /></td>
-                    <td className='table-data'>approved</td>
+        <div className='dashboard-container'>
+            <h1>Dashboard</h1>
+            <Table striped bordered hover className='table-style'>
+            <thead>
+                <tr>
+                <th>Claim Id</th>
+                <th>Project Id</th>
+                <th>Currency</th>
+                <th>Status of Claim</th>
                 </tr>
-                )
-            }
-        </tbody>
-        </Table>
-        
-        
-        <Button href="/home">Create new claim</Button> 
-    </>
-    
+            </thead>
+            <tbody>
+                {
+                    allClaims.map( (claim,key) =>
+                    <tr key={key}>
+                        <td className='table-data'>{claim.name }</td>
+                        <td className='table-data'>{claim.capital }</td>
+                        <td className='table-data'><img width='20px' height='10px'
+                                src={claim.media.flag} alt="flag" /></td>
+                        <td className='status' style={{color: getColor("Approved")}}>Approved</td>
+                        <td className='table-data'><Link to={{
+                            pathname: '/home',
+                            state: {employeeId: 100, claimId: 200, currencyId: 'MYR', projectId: 300}
+                            }} >Details</Link></td>
+                    </tr>
+                    )
+                }
+            </tbody>
+            </Table>
+            <Button href="/home">Create new claim</Button> 
+        </div>
     );
 }
